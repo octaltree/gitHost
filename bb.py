bfdbbede208912d582de068ccdca7fc81e26ccf5
octaltree@yassan <octaltree@gmail.com>
2016-01-17 23:22:34 +0900

    writing

--- bb.py
+++ bb.py
@@ -6,10 +6,48 @@ import http.client
 import json
 from enum import Enum
 
+bucketkey = "7bbXmBy2GsC9arwn6A"
+bucketsecret = "KfXWYDhsdLkNbC52v25ZVgDXhJV2rWTR"
+buckettoken = "RPt7yEvBw6HdRf7rFY"
+
 # main :: IO Int
 def main():
     args = sys.argv[1:]
-    undefined
+    useragent = "gitHost"
+    method = "GET"
+    hostname = "bitbucket.org"
+    path = "/site/oauth2/authorize"
+    query = urllib.parse.urlencode({
+        "client_id": bucketkey,
+        "response_type": "code"})
+    header = {
+            "User-Agent": useragent}
+    url = urllib.parse.urlunparse(("", "", path, "", query, ""))
+    #conn = http.client.HTTPSConnection(hostname)
+    #conn.request(method, url, headers = header)
+    #response = conn.getresponse()
+    #print(response.read())
+    #print(str(response.status) + "  " + response.reason)
+    getBucketRepos()
+    return 0
+
+def getBucketRepos():
+    hostname = "api.bitbuket.org"
+    pathbase = "/2.0/repositories"
+    path = pathbase + "/octaltree"
+    url = urllib.parse.urlunparse(("", "", path, "", "", ""))
+    header = {
+            "User-Agent": "getHost"}
+    method = "GET"
+
+    print(url)
+    conn = http.client.HTTPSConnection(hostname)
+    conn.request(method, url, headers = header)
+    response = conn.getresponse()
+    print(response.read())
+    print(str(response.status) + "  " + response.reason)
+
+
 
 # undefined :: a
 undefined = None
