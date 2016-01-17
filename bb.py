fb9bbf16d4805daa198dd811689fcc6c3c57d5bd
octaltree@yassan <octaltree@gmail.com>
2016-01-17 22:15:08 +0900

    add bitbuckettest

---
+++ bb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+# main :: IO Int
+def main():
+    args = sys.argv[1:]
+    undefined
+
+# undefined :: a
+undefined = None
+
+if __name__ == "__main__" :
+  exit(main())
+
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et fdm=indent fdl=0 fdn=1:
+# vim:cinw=if,elif,else,for,while,try,except,finally,def,class:
