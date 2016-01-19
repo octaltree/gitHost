0d80e41e5d4aa94fe012dba13c99b185d689ae70
octaltree@yassan <octaltree@gmail.com>
2016-01-19 22:55:53 +0900

    get repolist

--- gl.py
+++ gl.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python3
 
+labkey="8ba20e67ea70d9622041588ecd50774ec56d005d1d363b9938803e7fe1b74179"
+labsecret="36b982ce8292ac99c755811b4010bf596c82172b3bc2968df8b86efdd5daca09"
+labtoken="YxP7jS3Ut7JhyxYDWKSz"
+
 # main :: IO Int
 def main():
     import sys
     args = sys.argv[1:]
+    test()
     return undefined
+
+def test():
+    import urllib.parse
+    import urllib.request
+    import urllib.error
+    url = "https://gitlab.com/api/v3/projects"
+    headers = {
+            "PRIVATE-TOKEN": "%s" % labtoken}
+    req = urllib.request.Request(url, headers = headers)
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read().decode('utf-8'))
+        print(response.getheaders())
+        print(response.status, response.reason)
+    except urllib.request.HTTPError as e:
+        print(e.read())
+
 
 # undefined :: a
 undefined = None
