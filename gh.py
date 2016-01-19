c4782a1c0f270cf26169e2ad964dafe716dc5f56
octaltree@yassan <octaltree@gmail.com>
2016-01-20 00:36:06 +0900

    test write success gh.py

--- gh.py
+++ gh.py
@@ -1,10 +1,59 @@
 #!/usr/bin/python3
 
+USERAGENT='gitHost'
+
+hubtoken = "8f32aae21b80d8644d3ed02fdec2e7b04031400f"
+
 # main :: IO Int
 def main():
     import sys
     args = sys.argv[1:]
+    #test()
+    newRepo('apitestsdf', False)
     return undefined
+
+def test():
+    import urllib.parse
+    import urllib.request
+    import urllib.error
+    url = "https://api.github.com/user/repos"
+    headers = {
+            "Authorization": "token %s" % hubtoken,
+            "User-Agent": "%s" % USERAGENT}
+    req = urllib.request.Request(url, headers = headers)
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read())
+        print(response.getheaders())
+        print(response.status, response.reason)
+    except urllib.request.HTTPError as e:
+        print(e.read())
+        print(e.getheaders())
+        print(e.status, e.reason)
+
+def newRepo(name, isprivate):
+    import urllib.parse
+    import urllib.request
+    import urllib.error
+    import json
+    url = "https://api.github.com/user/repos"
+    headers = {
+            "Authorization": "token %s" % hubtoken,
+            "User-Agent": "%s" % USERAGENT}
+    data = json.dumps({
+        "name": name,
+        "private": isprivate}).encode('utf-8')
+    req = urllib.request.Request(url, data, headers)
+    try:
+        response = urllib.request.urlopen(req)
+        print(response.read())
+        print(response.getheaders())
+        print(response.status, response.reason)
+    except urllib.error.HTTPError as e:
+        print(e.read())
+        print(e.getheaders())
+        print(e.status, e.reason)
+
 
 # undefined :: a
 undefined = None
