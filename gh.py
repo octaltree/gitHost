e585d88f88824ca724b02de46e9e76ed69b725f2
octaltree@yassan <octaltree@gmail.com>
2016-01-20 00:18:08 +0900

    add gh.py

---
+++ gh.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# main :: IO Int
+def main():
+    import sys
+    args = sys.argv[1:]
+    return undefined
+
+# undefined :: a
+undefined = None
+
+if __name__ == "__main__" :
+  exit(main())
+
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et fdm=indent fdl=0 fdn=1:
+# vim:cinw=if,elif,else,for,while,try,except,finally,def,class:
